3ec128c9e4b71f5a38145d957c0bad9069216277
CarlsonZhuo <carlsonzhuo@gmail.com>
2017-03-22 14:33:45 +0800

    SAG done

--- Alg_SAG.cpp
+++ Alg_SAG.cpp
@@ -8,266 +8,185 @@
 #include "utilities_sparse.h"
 
 /*
-	USAGE:
-	hist = SAG(w, Xt, y, lambda, stepsize, iVals);
-	==================================================================
-	INPUT PARAMETERS:
-	w (d x 1) - initial point; updated in place
-	Xt (d x n) - data matrix; transposed (data points are columns); real
-	y (n x 1) - labels; in {-1,1}
-	lambda - scalar regularization param
-	stepSize - a step-size
-	iVals(iters x 1) - sequence of examples to choose, between 0 and (n-1)
-	==================================================================
-	OUTPUT PARAMETERS:
-	hist = array of function values after each outer loop.
-	Computed ONLY if asked for output in MATALB.
+    USAGE:
+    hist = SAG(w, Xt, y, lambda, stepsize, iVals);
+    ==================================================================
+    w (d x 1) - initial point; updated in place
+    Xt (d x n) - data matrix; transposed (data points are columns); real
+    y (n x 1) - labels; in {-1,1}
+    iVals(iters x 1) - sequence of examples to choose, between 0 and (n-1)
 */
 
-/// SAG_dense runs the S2GD algorithm for solving regularized 
-/// logistic regression on dense data provided
 /// nlhs - number of output parameters requested
-///		   if set to 1, function values are computed
+///        if set to 1, function values are computed
 /// *prhs[] - array of pointers to the input arguments
 mxArray* SAG_dense(int nlhs, const mxArray *prhs[]) {
 
-	//////////////////////////////////////////////////////////////////
-	/// Declare variables ////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+    // Other variables
+    long i, k; // Some loop indexes
+    long long idx; // For choosing indexes
+    bool evalf = false; // set to true if function values should be evaluated
 
-	// Input variables
-	double *w, *Xt, *y;
-	double lambda, stepSize;
-	long long *iVals;
+    double *hist; // Used to store function value at points in history
 
-	// Other variables
-	long i, k; // Some loop indexes
-	long n, d; // Dimensions of problem
-	long iters; // Number of outer loops
-	long long idx; // For choosing indexes
-	double sigmoid; // Scalar value of the derivative of sigmoid function
-	double *sigmoidold; // Array of old sigmoids
-	double *g; // Direction of update
-	bool evalf = false; // set to true if function values should be evaluated
+    mxArray *plhs; // History array to return if needed
 
-	double *hist; // Used to store function value at points in history
+    double *w       = mxGetPr(prhs[0]); // The variable to be learned
+    double *Xt      = mxGetPr(prhs[1]); // Data matrix (transposed)
+    double *y       = mxGetPr(prhs[2]); // Labels
+    double lambda   = mxGetScalar(prhs[3]); // Regularization parameter
+    double stepSize = mxGetScalar(prhs[4]); // Step-size (constant)
+    long long *iVals= (long long*)mxGetPr(prhs[5]); // Sampled indexes (sampled in advance)
 
-	mxArray *plhs; // History array to return if needed
+    long d          = mxGetM(prhs[1]); // Number of features, or dimension of problem
+    long n          = mxGetN(prhs[1]); // Number of samples, or data points
+    long iters      = mxGetM(prhs[5]); // Number of outer iterations
 
-	//////////////////////////////////////////////////////////////////
-	/// Process input ////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+    if (nlhs == 1) {
+        evalf = true;
+    }
+    if (!mxIsClass(prhs[5], "int64"))
+        mexErrMsgTxt("iVals must be int64");
+    
+    double *sigmoidold  = new double[n];
+    double sigmoid;
+    double *g           = new double[d];
+    for (i = 0; i < n; i++) { sigmoidold[i] = 0; }
+    for (i = 0; i < d; i++) { g[i] = 0; }
+    if (evalf == true) {
+        plhs = mxCreateDoubleMatrix((long)floor((double)iters / (2 * n)) + 1, 1, mxREAL);
+        hist = mxGetPr(plhs);
+    }
 
-	w = mxGetPr(prhs[0]); // The variable to be learned
-	Xt = mxGetPr(prhs[1]); // Data matrix (transposed)
-	y = mxGetPr(prhs[2]); // Labels
-	lambda = mxGetScalar(prhs[3]); // Regularization parameter
-	stepSize = mxGetScalar(prhs[4]); // Step-size (constant)
-	iVals = (long long*)mxGetPr(prhs[5]); // Sampled indexes (sampled in advance)
-	if (nlhs == 1) {
-		evalf = true;
-	}
+    //////////////////////////////////////////////////////////////////
+    /// The SAG algorithm ////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
 
-	if (!mxIsClass(prhs[5], "int64"))
-		mexErrMsgTxt("iVals must be int64");
-	
-	//////////////////////////////////////////////////////////////////
-	/// Get problem related constants ////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+    for (k = 0; k < iters; k++)
+    {
+        // Evaluate function value from time to time if output requested
+        if (evalf == true && k % (2 * n) == 0) {
+            hist[(long)floor((double)k / (2 * n))] = compute_function_value(w, Xt, y, n, d, lambda);
+        }
 
-	d = mxGetM(prhs[1]); // Number of features, or dimension of problem
-	n = mxGetN(prhs[1]); // Number of samples, or data points
-	iters = mxGetM(prhs[5]); // Number of outer iterations
+        idx = *(iVals++); // Sample function and move pointer
 
-	//////////////////////////////////////////////////////////////////
-	/// Initialize some values ///////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+        // Compute current scalar sigmoid value of sampled function
+        sigmoid = compute_sigmoid(Xt + d*idx, w, y[idx], d);
 
-	// Allocate memory to store full gradient and point in which it
-	// was computed
-	sigmoidold = new double[n];
-	g = new double[d];
-	for (i = 0; i < n; i++) { sigmoidold[i] = 0; }
-	for (i = 0; i < d; i++) { g[i] = 0; }
-	if (evalf == true) {
-		plhs = mxCreateDoubleMatrix((long)floor((double)iters / (2 * n)) + 1, 1, mxREAL);
-		hist = mxGetPr(plhs);
-	}
+        // Update the aggregate gradient
+        for (i = 0; i < d; i++) {
+            g[i] += Xt[d*idx + i] * (sigmoid - sigmoidold[idx]) / n;
+        }
+        // Save the last sigmoid value for this function
+        sigmoidold[idx] = sigmoid;
+        
+        // Update the test point
+        update_test_point_dense_SAG(w, g, d, stepSize, lambda);
+    }
 
-	//////////////////////////////////////////////////////////////////
-	/// The SAG algorithm ////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+    if (evalf == true) {
+        hist[(long)floor((double)iters / (2 * n))] = compute_function_value(w, Xt, y, n, d, lambda);
+    }
 
-	for (k = 0; k < iters; k++)
-	{
-		// Evaluate function value from time to time if output requested
-		if (evalf == true && k % (2 * n) == 0) {
-			hist[(long)floor((double)k / (2 * n))] = compute_function_value(w, Xt, y, n, d, lambda);
-		}
+    delete[] sigmoidold;
+    delete[] g;
 
-		idx = *(iVals++); // Sample function and move pointer
-
-		// Compute current scalar sigmoid value of sampled function
-		sigmoid = compute_sigmoid(Xt + d*idx, w, y[idx], d);
-
-		// Update the aggregate gradient
-		for (i = 0; i < d; i++) {
-			g[i] += Xt[d*idx + i] * (sigmoid - sigmoidold[idx]) / n;
-		}
-		// Save the last sigmoid value for this function
-		sigmoidold[idx] = sigmoid;
-		
-		// Update the test point
-		update_test_point_dense_SAG(w, g, d, stepSize, lambda);
-	}
-
-	if (evalf == true) {
-		hist[(long)floor((double)iters / (2 * n))] = compute_function_value(w, Xt, y, n, d, lambda);
-	}
-
-	//////////////////////////////////////////////////////////////////
-	/// Free some memory /////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-	delete[] sigmoidold;
-	delete[] g;
-
-	if (evalf == true) { return plhs; }
-	else { return 0; }
+    if (evalf == true) { return plhs; }
+    else { return 0; }
 
 }
 
-/// SAG_sparse runs the SAG algorithm for solving regularized 
-/// logistic regression on sparse data provided
 /// nlhs - number of output parameters requested
-///		   if set to 1, function values are computed
+///        if set to 1, function values are computed
 /// *prhs[] - array of pointers to the input arguments
 mxArray* SAG_sparse(int nlhs, const mxArray *prhs[]) {
 
-	//////////////////////////////////////////////////////////////////
-	/// Declare variables ////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+    long i, j, k; // Some loop indexes
+    long long idx; // For choosing indexes
+    bool evalf = false; // set to true if function values should be evaluated
 
-	// Input variables
-	double *w, *Xt, *y;
-	double lambda, stepSize;
-	long long *iVals;
+    double *hist;// Used to store function value at points in history
 
-	// Other variables
-	long i, j, k; // Some loop indexes
-	long n, d; // Dimensions of problem
-	long iters; // Number of outer loops
-	long long idx; // For choosing indexes
-	double sigmoid; // Scalar value of the derivative of sigmoid function
-	double *sigmoidold; // Array of old sigmoids
-	double *g; // Direction of update
-	bool evalf = false; // set to true if function values should be evaluated
+    mxArray *plhs; // History array to return if needed
 
-	long *last_seen; // used to do lazy "when needed" updates
-	double *hist;// Used to store function value at points in history
+    double *w       = mxGetPr(prhs[0]); // The variable to be learned
+    double *Xt      = mxGetPr(prhs[1]); // Data matrix (transposed)
+    double *y       = mxGetPr(prhs[2]); // Labels
+    double lambda   = mxGetScalar(prhs[3]); // Regularization parameter
+    double stepSize = mxGetScalar(prhs[4]); // Step-size (constant)
+    long long *iVals= (long long*)mxGetPr(prhs[5]); // Sampled indexes (sampled in advance)
+    long d          = mxGetM(prhs[1]); // Number of features, or dimension of problem
+    long n          = mxGetN(prhs[1]); // Number of samples, or data points
+    long iters      = mxGetM(prhs[5]); // Number of outer iterations
+    mwIndex *jc     = mxGetJc(prhs[1]); // pointers to starts of columns of Xt
+    mwIndex *ir     = mxGetIr(prhs[1]); // row indexes of individual elements of Xt
 
-	mwIndex *ir, *jc; // Used to access nonzero elements of Xt
-	mxArray *plhs; // History array to return if needed
+    if (nlhs == 1) {
+        evalf = true;
+    }
+    if (!mxIsClass(prhs[5], "int64"))
+        mexErrMsgTxt("iVals must be int64");
 
-	//////////////////////////////////////////////////////////////////
-	/// Process input ////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+    double *sigmoidold  = new double[n];
+    double sigmoid;
+    double *g           = new double[d];
+    long *last_seen     = new long[d];
+    for (i = 0; i < n; i++) { sigmoidold[i] = 0; }
+    for (i = 0; i < d; i++) { g[i] = 0; }
+    for (i = 0; i < d; i++) { last_seen[i] = 0; }
+    if (evalf == true) {
+        plhs = mxCreateDoubleMatrix((long)floor((double)iters / (2 * n)) + 1, 1, mxREAL);
+        hist = mxGetPr(plhs);
+    }
 
-	w = mxGetPr(prhs[0]); // The variable to be learned
-	Xt = mxGetPr(prhs[1]); // Data matrix (transposed)
-	y = mxGetPr(prhs[2]); // Labels
-	lambda = mxGetScalar(prhs[3]); // Regularization parameter
-	stepSize = mxGetScalar(prhs[4]); // Step-size (constant)
-	iVals = (long long*)mxGetPr(prhs[5]); // Sampled indexes (sampled in advance)
-	if (nlhs == 1) {
-		evalf = true;
-	}
+    //////////////////////////////////////////////////////////////////
+    /// The SAG algorithm ////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
 
-	if (!mxIsClass(prhs[5], "int64"))
-		mexErrMsgTxt("iVals must be int64");
-	
-	//////////////////////////////////////////////////////////////////
-	/// Get problem related constants ////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+    for (k = 0; k < iters; k++)
+    {
+        // Evaluate function value from time to time if output requested
+        if (evalf == true && k % (2 * n) == 0) {
+            hist[(long)floor((double)k / (2 * n))] = compute_function_value_sparse(w, Xt, y, n, d, lambda, ir, jc);
+        }
 
-	d = mxGetM(prhs[1]); // Number of features, or dimension of problem
-	n = mxGetN(prhs[1]); // Number of samples, or data points
-	iters = mxGetM(prhs[5]); // Number of outer iterations
-	jc = mxGetJc(prhs[1]); // pointers to starts of columns of Xt
-	ir = mxGetIr(prhs[1]); // row indexes of individual elements of Xt
+        idx = *(iVals++); // Sample function and move pointer
 
-	//////////////////////////////////////////////////////////////////
-	/// Initialize some values ///////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+        // Update what we didn't in last few iterations
+        // Only relevant coordinates
+        update_test_point_sparse_SAG(w, g, last_seen, jc[idx + 1] - jc[idx], stepSize, lambda, ir + jc[idx], k);
 
-	// Allocate memory to store full gradient and point in which it
-	// was computed
-	sigmoidold = new double[n];
-	g = new double[d];
-	last_seen = new long[d];
-	for (i = 0; i < n; i++) { sigmoidold[i] = 0; }
-	for (i = 0; i < d; i++) { g[i] = 0; }
-	for (i = 0; i < d; i++) { last_seen[i] = 0; }
-	if (evalf == true) {
-		plhs = mxCreateDoubleMatrix((long)floor((double)iters / (2 * n)) + 1, 1, mxREAL);
-		hist = mxGetPr(plhs);
-	}
+        // Compute current scalar sigmoid value of sampled function
+        sigmoid = compute_sigmoid_sparse(Xt + jc[idx], w, y[idx], jc[idx + 1] - jc[idx], ir + jc[idx]);
+        
+        // Update the aggregate gradient
+        for (j = jc[idx]; j < jc[idx + 1]; j++) {
+            g[ir[j]] += Xt[j] * (sigmoid - sigmoidold[idx]) / n;
+        }
+        // Save the last sigmoid value for this function
+        sigmoidold[idx] = sigmoid;
 
-	//////////////////////////////////////////////////////////////////
-	/// The SAG algorithm ////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
+        // Do NOT update test point. Relevant coordinates will be 
+        // updated at the beginning of iterations when they will be needed.
+    }
 
-	for (k = 0; k < iters; k++)
-	{
-		// Evaluate function value from time to time if output requested
-		if (evalf == true && k % (2 * n) == 0) {
-			hist[(long)floor((double)k / (2 * n))] = compute_function_value_sparse(w, Xt, y, n, d, lambda, ir, jc);
-		}
+    // Finish lazy updates
+    for (i = 0; i < d; i++) {
+        w[i] -= stepSize * (iters - last_seen[i]) * (g[i] + lambda * w[i]);
+    }
 
-		idx = *(iVals++); // Sample function and move pointer
+    // Evaluate the final function value
+    if (evalf == true) {
+        hist[(long)floor((double)iters / (2 * n))] = compute_function_value_sparse(w, Xt, y, n, d, lambda, ir, jc);
+    }
 
-		// Update what we didn't in last few iterations
-		// Only relevant coordinates
-		update_test_point_sparse_SAG(w, g, last_seen, jc[idx + 1] - jc[idx], stepSize, lambda, ir + jc[idx], k);
+    delete[] sigmoidold;
+    delete[] g;
+    delete[] last_seen;
 
-		// Compute current scalar sigmoid value of sampled function
-		sigmoid = compute_sigmoid_sparse(Xt + jc[idx], w, y[idx], jc[idx + 1] - jc[idx], ir + jc[idx]);
-		
-		// Update the aggregate gradient
-		for (j = jc[idx]; j < jc[idx + 1]; j++) {
-			g[ir[j]] += Xt[j] * (sigmoid - sigmoidold[idx]) / n;
-		}
-		// Save the last sigmoid value for this function
-		sigmoidold[idx] = sigmoid;
-
-		// Do NOT update test point. Relevant coordinates will be 
-		// updated at the beginning of iterations when they will be needed.
-	}
-
-	// Finish lazy updates
-	for (i = 0; i < d; i++) {
-		w[i] -= stepSize * (iters - last_seen[i]) * (g[i] + lambda * w[i]);
-	}
-
-	// Evaluate the final function value
-	if (evalf == true) {
-		hist[(long)floor((double)iters / (2 * n))] = compute_function_value_sparse(w, Xt, y, n, d, lambda, ir, jc);
-	}
-
-	//////////////////////////////////////////////////////////////////
-	/// Free some memory /////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-	delete[] sigmoidold;
-	delete[] g;
-	delete[] last_seen;
-
-	//////////////////////////////////////////////////////////////////
-	/// Return value /////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-	if (evalf == true) { return plhs; }
-	else { return 0; }
+    if (evalf == true) { return plhs; }
+    else { return 0; }
 
 }
 
@@ -280,12 +199,12 @@ mxArray* SAG_sparse(int nlhs, const mxArray *prhs[]) {
 /// http://www.mathworks.co.uk/help/matlab/matlab_external/gateway-routine.html
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
-	// First determine, whether the data matrix is stored in sparse format.
-	// If it is, use more efficient algorithm
-	if (mxIsSparse(prhs[1])) {
-		plhs[0] = SAG_sparse(nlhs, prhs);
-	}
-	else {
-		plhs[0] = SAG_dense(nlhs, prhs);
-	}
+    // First determine, whether the data matrix is stored in sparse format.
+    // If it is, use more efficient algorithm
+    if (mxIsSparse(prhs[1])) {
+        plhs[0] = SAG_sparse(nlhs, prhs);
+    }
+    else {
+        plhs[0] = SAG_dense(nlhs, prhs);
+    }
 }
